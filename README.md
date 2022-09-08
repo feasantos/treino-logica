835d246b250170af562575bb477a4c3c7003419b
feasantos <felipe_fas87@hotmail.com>
2022-09-08 19:35:23 -0300

    lacos km

--- README.md
+++ README.md
@@ -202,3 +202,4 @@
 
 ## Laços de Repetição
 
+    
---
+++ lacosrepeticao.js
@@ -0,0 +1,10 @@
+/* Laços de repetição */
+
+//for ([expressaoInicial]; [condicao]; [incremento])
+// Fazer revisão do carro aos 10k//
+
+var km;
+var revisao = 3;
+for ( km = 0; km <= revisao; km++){
+    console.log ("apenas" + km +"kms então pode rodar");
+}
