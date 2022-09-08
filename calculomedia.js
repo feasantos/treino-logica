7bd68c41dacebd7de0fba9ef62c6e6897cd7d407
feasantos <felipe_fas87@hotmail.com>
2022-09-08 19:17:47 -0300

    Aplicação calculo media

---
+++ calculomedia.js
@@ -0,0 +1,14 @@
+/* Calculo Média Alunos */
+
+var nota1 = 10;
+var nota2 = 9;
+var nota3 = 8;
+var nota4 = 3;
+var nota5 = 5;
+
+var media = (nota1 + nota2 + nota3 + nota4 + nota5)/ 5;
+if (media > 8){
+    console.log("Aluno aprovado")
+} else {
+    console.log("Aluno em recuperação")
+}
